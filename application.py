13649a1a2c8f329274aca50fa52c786d317d3e1e
Soham Antre <sohamantre007@gmail.com>
2026-03-12 03:17:27 +0530

    add hello route

--- application.py
+++ application.py
@@ -4,22 +4,23 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-application=Flask(__name__)
-app=application
+application = Flask(__name__)
+app = application
 
-
-## import ridge regressor and standard scalar pickle
-
-ridge_model=pickle.load(open('models/ridge.pkl','rb'))
-Standard_Scaler=pickle.load(open('models/scaler.pkl','rb'))
+ridge_model = pickle.load(open('models/ridge.pkl','rb'))
+Standard_Scaler = pickle.load(open('models/scaler.pkl','rb'))
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/hello")
+def hello():
+    return "Hello Soham"
+
 @app.route("/test")
 def test():
-    return "Server working"    
+    return "Server working"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
